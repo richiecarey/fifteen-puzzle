7aa081894f747a6578ac87d7d63f5c7881965634
Richie Carey <richiecarey@gmail.com>
2019-12-29 18:24:16 -0500

    Updates.

--- src/Tile.js
+++ src/Tile.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "./Tile.css";
 
 export default function Tile(props) {
-  //const [point, setPoint] = useState(props.point);
-  //console.log(point);
   return (
     <button
       className={props.num ? "square" : "blank"}
