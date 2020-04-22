97a398941a3042abf2b595fd4e17dedd7b78bd26
Richie Carey <richiecarey@gmail.com>
2020-04-22 17:50:25 -0400

    Updates.

--- src/FifteenPuzzle.js
+++ src/FifteenPuzzle.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Tile from "./Tile";
-import "./FifteenPuzzle.css";
 import legalMove from "./utils";
+import "./FifteenPuzzle.css";
 
 export default function FifteenPuzzle() {
   var tiles = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
